aa178fe391be9b33b4fd5b80acaf8354379de86f
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-03-12 12:34:12 +0300

    Update Student.java

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,66 +1,52 @@
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.TreeSet;
 
 public class Student implements Comparable<Student> {
     private String name;
     private int age;
+    private String email;
     private long phoneNumber;
-    private static ArrayList<Student> listStudents = new ArrayList<>();
+    private static TreeSet<Student> setStudents = new TreeSet<>();
 
-    public Student(String name, int age, Long phoneNumber) {
+    public Student(String name, int age, String email, long phoneNumber) {
         this.name = name;
         this.age = age;
+        this.email = email;
         this.phoneNumber = phoneNumber;
-        listStudents.add(this);
+        setStudents.add(this);
+    }
+
+    public static void getListAllStudents() {
+        for (Student currentStudent : setStudents) {
+            System.out.println(currentStudent);
+        }
+    }
+
+    public static void removeSelectedStudent(long phoneNumber) {
+        Iterator<Student> studentsIterator = setStudents.iterator();
+        while (studentsIterator.hasNext()) {
+            Student selectedStudent = studentsIterator.next();
+            if (selectedStudent.getPhoneNumber() == phoneNumber) {
+                studentsIterator.remove();
+            }
+        }
     }
 
     public String getName() {
         return name;
     }
 
-    public Integer getAge() {
+    public int getAge() {
         return age;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public Long getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public static void printAllStudents() {
-        for (Student currentName : listStudents) {
-            System.out.println("Parameters student \"" + currentName + "\", " +
-                    "hash code student \"" + currentName.hashCode() + "\"");
-        }
-    }
-
-    public static void removeStudent(long phoneNumber) {
-        Iterator<Student> studentIterator = listStudents.iterator();
-        while (studentIterator.hasNext()) {
-            Student currentStudent = studentIterator.next();
-            if (currentStudent.getPhoneNumber() == phoneNumber) {
-                System.out.println("Студент " + currentStudent + " удалён!");
-                studentIterator.remove();
-            }
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return age == student.age && Objects.equals(name, student.name) && Objects.equals(phoneNumber, student.phoneNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age, phoneNumber);
-    }
-
-    @Override
-    public int compareTo(Student currentStudent) {
-        return getPhoneNumber().compareTo(currentStudent.getPhoneNumber());
     }
 
     @Override
@@ -68,7 +54,26 @@ public class Student implements Comparable<Student> {
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && phoneNumber == student.phoneNumber && Objects.equals(name, student.name) && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, email, phoneNumber);
+    }
+
+    @Override
+    public int compareTo(Student currentStudent) {
+        return this.getPhoneNumber().compareTo(currentStudent.getPhoneNumber());
     }
 }
